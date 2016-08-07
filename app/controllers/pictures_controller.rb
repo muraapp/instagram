f9e66febb95b6ca7efd6c0e701820c14ce5f84ed
Kazu Murata <muraapp@Kazu-no-MacBook-Pro.local>
2016-08-07 14:10:27 +0900

    first commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,42 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only:[:edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(pictures_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "写真を投稿しました！"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @picture.update(pictures_params)
+    redirect_to pictures_path, notice: "写真を更新しました！"
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "写真を削除しました！"
+  end
+
+  private
+    def pictures_params
+      params.require(:picture).permit(:content)
+    end
+
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+end
