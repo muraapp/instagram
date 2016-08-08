595762c3003930a35578ef7109e76716571bcba6
Kazu Murata <muraapp@Kazu-no-MacBook-Pro.local>
2016-08-08 20:02:48 +0900

    fixed1

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -8,7 +8,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_picture(picture)
     @picture = picture
 
-    mail to: "bergsonisme@gmail.com"
+    mail to: "bergsonisme@gmail.com",
           subject: '【Instagram】写真が投稿されました '
   end
 end
